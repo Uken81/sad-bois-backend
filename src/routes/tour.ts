a1acd356209d06edec9696ff31cfc9e4ecd24148
Brendan Hurd <brendanhurd@gmail.com>
2024-02-26 17:36:21 +1100

    Add try/catch blocks to all routes in /tour.

--- src/routes/tour.ts
+++ src/routes/tour.ts
@@ -6,39 +6,51 @@ import { isResultEmpty } from '../Utils/isResultEmpty';
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-  const query = 'SELECT * FROM tour ORDER BY date DESC;';
-  pool?.query(query, (err: Error | null, results: QueryResult) => {
-    if (err) {
-      console.error('Error executing query: ', err);
-      res.status(500).json({
-        error: 'Database error occured',
-        details: err.message
-      });
-    }
+  try {
+    const query = 'SELECT * FROM tour ORDER BY date DESC;';
+    pool?.query(query, (err: Error | null, results: QueryResult) => {
+      if (err) {
+        console.error('Error querying the database:', err);
+        return res.status(500).json({
+          message: 'Server error'
+        });
+      }
 
-    if (isResultEmpty(results)) {
-      return res.status(404).json({
-        error: 'No shows found'
-      });
-    }
+      if (isResultEmpty(results)) {
+        return res.status(404).json({
+          message: 'No shows found'
+        });
+      }
 
-    res.status(200).json(results.rows);
-  });
+      res.status(200).json(results.rows);
+    });
+  } catch (error) {
+    console.error('Unexpected error fetching news: ', error);
+    return res.status(500).json({
+      message: 'Internal Server Error. Please try again later.'
+    });
+  }
 });
 
 router.get('/latest', (req: Request, res: Response) => {
-  const query = 'SELECT * FROM tour ORDER BY date DESC LIMIT 4;';
-  pool?.query(query, (err: Error, results: QueryResult) => {
-    if (err) {
-      console.error('Error executing query: ', err);
-      res.status(500).json({
-        error: 'Database error occured',
-        details: err.message
-      });
-    }
+  try {
+    const query = 'SELECT * FROM tou ORDER BY date DESC LIMIT 4;';
+    pool?.query(query, (err: Error, results: QueryResult) => {
+      if (err) {
+        console.error('Error querying the database:', err);
+        return res.status(500).json({
+          message: 'Server error'
+        });
+      }
 
-    res.status(200).json(results.rows);
-  });
+      res.status(200).json(results.rows);
+    });
+  } catch (error) {
+    console.error('Unexpected error fetching news: ', error);
+    return res.status(500).json({
+      message: 'Internal Server Error. Please try again later.'
+    });
+  }
 });
 
 export default router;
