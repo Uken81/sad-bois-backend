11b42b3da1f420b202a05adb985bf06090496f15
Brendan Hurd <brendanhurd@gmail.com>
2024-03-14 12:27:13 +1100

    Refactor CheckIfExistingCustomer middleware to a function and change query to search for matching id's.

---
+++ src/routes/ProcessOrder/Utils/checkIfExistingCustomer.ts
@@ -0,0 +1,30 @@
+import { pool } from '../../../server';
+import { QueryResult } from 'pg';
+import { UserCredentials } from '../../../middlewares/getUserCredentials';
+
+export const checkIfExistingCustomer = async (
+  userCredentials: UserCredentials
+): Promise<boolean> => {
+  return new Promise((resolve, reject) => {
+    if (!userCredentials.isLoggedIn) {
+      resolve(false);
+    }
+
+    const query = 'SELECT EXISTS (SELECT 1 FROM customers WHERE id = $1) AS conditionMet;';
+
+    pool?.query(query, [userCredentials.id], (err: Error | null, results: QueryResult) => {
+      if (err) {
+        return reject(new Error(`Error executing query: ${err}`));
+      }
+
+      const row = results.rows[0];
+      const conditionMet: boolean = row.conditionmet;
+      if (row.conditionmet === undefined || typeof conditionMet !== 'boolean') {
+        return reject(new Error('Incorrect value returned from query'));
+      }
+
+      console.log('condition met: ', conditionMet);
+      resolve(conditionMet);
+    });
+  });
+};
--- src/routes/ProcessOrder/process-order.ts
+++ src/routes/ProcessOrder/process-order.ts
@@ -6,7 +6,7 @@ import {
 import { calculateOrderTotal } from './Utils/calculateOrderTotal';
 import { CartType, CustomerType, OrderDataType, ShippingType } from '../../Types/checkoutTypes';
 import { createOrder } from './Utils/createOrder';
-import { checkIfExistingCustomer } from '../../middlewares/checkIfExistingCustomer';
+import { checkIfExistingCustomer } from './Utils/checkIfExistingCustomer';
 import { insertCustomer } from './Utils/insertCustomer';
 import { v4 as uuidv4 } from 'uuid';
 import { insertOrder } from './Utils/insertOrder';
@@ -15,45 +15,55 @@ import { getUserCredentials } from '../../middlewares/getUserCredentials';
 const router = express.Router();
 
 router.post('/', getUserCredentials, async (req: Request, res: Response) => {
-  const userCredentials = req.userCredentials;
-  console.log('Creds: ', userCredentials);
   try {
-    // const cardDetails: CardDetailsType = req.body.formValues;
+    const userCredentials = req.userCredentials;
+    if (!userCredentials) {
+      console.error('userCredentials is null or undefined');
+      return res
+        .status(500)
+        .json({ message: 'Internal server error: user credentials processing failed.' });
+    }
+
+    console.log('Creds: ', userCredentials);
+    const isExistingCustomer = await checkIfExistingCustomer(userCredentials);
+    console.log('IEC: ', isExistingCustomer);
+
     // const customerDetails: CustomerType = req.body.customer;
-    // const cart: CartType = req.body.cart;
-    // const shippingData: ShippingType = req.body.selectedShipping;
-    // if (!cardDetails || !customerDetails || !cart || !shippingData) {
-    //   console.error(
-    //     `Missing required query paramater in processing order for ${customerDetails.email}`
-    //   );
-    //   return res.status(400).json({ message: 'Form details error' });
-    // }
-    // const validationResults = validateCardDetails(cardDetails);
-    // if (!validationResults.success) {
-    //   console.error(`Could not validate card for ${customerDetails.email}`);
-    //   return res
-    //     .status(403)
-    //     .json({ type: validationResults.type, message: validationResults.message });
-    // }
-    // const isExistingCustomer: boolean | undefined = req.isExistingCustomer;
-    // console.log('isEC', isExistingCustomer);
-    // const newCustomerId = !isExistingCustomer ? uuidv4() : null;
-    // if (!isExistingCustomer) {
+    //   const cardDetails: CardDetailsType = req.body.formValues;
+    //   const cart: CartType = req.body.cart;
+    //   const shippingData: ShippingType = req.body.selectedShipping;
+    //   if (!cardDetails || !customerDetails || !cart || !shippingData) {
+    //     console.error(
+    //       `Missing required query paramater in processing order for ${customerDetails.email}`
+    //     );
+    //     return res.status(400).json({ message: 'Form details error' });
+    //   }
+    //   const validationResults = validateCardDetails(cardDetails);
+    //   if (!validationResults.success) {
+    //     console.error(`Could not validate card for ${customerDetails.email}`);
+    //     return res
+    //       .status(403)
+    //       .json({ type: validationResults.type, message: validationResults.message });
+    //   }
+    //   // const isExistingCustomer: boolean | undefined = req.isExistingCustomer;
+    //   // console.log('isEC', isExistingCustomer);
+    //   const customerId = userCredentials?.isLoggedIn ? userCredentials.id : uuidv4();
+    //   if (!isExistingCustomer) {
+    //     //test errors
+    //     await insertCustomer(customerDetails, newCustomerId);
+    //   }
+    //   const orderTotal = await calculateOrderTotal(cart, shippingData);
+    //   const orderData: OrderDataType = {
+    //     customerDetails,
+    //     cart,
+    //     shippingData
+    //   };
+    //   const customerOrder = createOrder(orderData, orderTotal);
     //   //test errors
-    //   await insertCustomer(customerDetails, newCustomerId);
-    // }
-    // const orderTotal = await calculateOrderTotal(cart, shippingData);
-    // const orderData: OrderDataType = {
-    //   customerDetails,
-    //   cart,
-    //   shippingData
-    // };
-    // const customerOrder = createOrder(orderData, orderTotal);
-    // //test errors
-    // await insertOrder(customerOrder);
-    // return res
-    //   .status(200)
-    //   .json({ message: 'Order processed successfully', orderSummary: customerOrder });
+    //   await insertOrder(customerOrder);
+    //   return res
+    //     .status(200)
+    //     .json({ message: 'Order processed successfully', orderSummary: customerOrder });
   } catch (error) {
     console.error('Unexpected error processing orders', error);
     return res.status(500).json({
